614bfc1a44770798a6cd6afc81dd98b0835daa8b
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-05 16:33:23 +0200

    Add const price and discounts js html select and assigned js var

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+const PRICE_POR_KM = 0.21;
+const SCONTO_OVER_65 = 40;
+const SCONTO_MINORENNI = 20;
 // - Chiedere nome e cognome dell'utente
 //     - dichiarare una variabile
 //     - assegnare alla variabile il valore restituito dall'input text
-let userDOMNominative = document.querySelector("#nominative")
+let userDOMNominative = document.querySelector("#nominative");
 
-console.log(userDOMNominative)
-console.log(typeof userDOMNominative)
+console.log(userDOMNominative);
+console.log(typeof userDOMNominative);
 // - Chiedere numero di chilometri che vuole percorrere 
 //     - dichiarare una variabile
 //     - assegnare alla variabile il valore restituito dall'input text
@@ -13,10 +16,13 @@ let userDOMKilometers = parseInt(document.getElementById("kilometers"));
 console.log(userDOMKilometers);
 console.log(typeof userDOMKilometers);
 console.dir(userDOMKilometers);
+// - Calcolare il prezzo totale del viaggio
+//     - Calcolare il prezzo standard in base ai chilomerti
+
+
 // - Chiedere il tipo di sconto in base all'età
 //     - dichiarare una variabile
 //     - assegnare alla variabile il valore restituito dal select
-// - Calcolare il prezzo totale del viaggio
-//     - Calcolare il prezzo standard in base ai chilomerti
-//     - Calcolare il gli sconti in base all'età
-//     - Calalcolare il prezzo finale eventualmente scontato
+let userDOMDiscount = document.getElementById("discount")
+console.log(userDOMDiscount);
+// - Calalcolare il prezzo finale eventualmente scontato
