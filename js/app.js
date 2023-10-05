44441128a293e23743f1288ad7ac0eb1d8457911
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-05 15:58:02 +0200

    add html km input and assigned js var

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// - Chiedere nome e cognome dell'utente
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restituito dall'input text
+
+// - Chiedere numero di chilometri che vuole percorrere 
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restituito dall'input text
+let userDOMKilometers = parseInt(document.getElementById("kilometers"))
+
+console.log(userDOMKilometers)
+console.log(typeof userDOMKilometers)
+console.dir(userDOMKilometers)
+// - Chiedere il tipo di sconto in base all'età
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restituito dal select
+// - Calcolare il prezzo totale del viaggio
+//     - Calcolare il prezzo standard in base ai chilomerti
+//     - Calcolare il gli sconti in base all'età
+//     - Calalcolare il prezzo finale eventualmente scontato
