c5be9befdca109bb4837765daf350778435e41cc
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-06 07:26:47 +0200

    Fix js and style card input

--- js/app.js
+++ js/app.js
@@ -9,33 +9,36 @@ btnDOMElement.addEventListener('click', function () {
     const SCONTO_OVER_65 = 40;
     const SCONTO_MINORENNI = 20;
 
-    // - Recupero il numero di chilometri che vuole percorrere 
-    //     - dichiarare una variabile
-    //     - assegnare alla variabile il valore restituito dall'input text
-    let userDOMKilometers = parseInt(document.getElementById("kilometers"));
-    console.log(userDOMKilometers);
-    console.log(typeof userDOMKilometers);
-    console.dir(userDOMKilometers);
-
-    // - Recupero il nome e cognome dell'utente
+// - Recupero il nome e cognome dell'utente
     //     - dichiarare una variabile
     //     - assegnare alla variabile il valore restituito dall'input text
     let userDOMNominative = document.querySelector("#nominative");
     console.log(userDOMNominative);
     console.log(typeof userDOMNominative);
 
+    // - Recupero il numero di chilometri che vuole percorrere 
+    //     - dichiarare una variabile
+    //     - assegnare alla variabile il valore restituito dall'input text
+    let userDOMKilometers = parseFloat(document.getElementById("kilometers").value);
+    console.log("numero chilometri: " + userDOMKilometers);
+    console.log(typeof userDOMKilometers);
+    
+    
     // - Recupero il valore dello sconto
-    let userDOMDiscount = document.getElementById("discount");
-    console.log(userDOMDiscount);
+    let userDOMDiscount = parseInt(document.getElementById("discount").value);
+    
+
+    console.log("valore select " + userDOMDiscount);
+    console.log(typeof userDOMDiscount)
 
     // - Calcolare il prezzo totale del viaggio
         //     - Calcolare il prezzo standard in base ai chilomerti
-        let standardPrice = (PRICE_POR_KM * userDOMKilometers);
-        console.log(standardPrice);
+    let standardPrice = (PRICE_POR_KM * userDOMKilometers);
+    console.log("prezzo standard " + standardPrice);
 
     // - Calalcolare il prezzo finale eventualmente scontato
-    let finalPrice
-
+    let discount
+    console.log("sconto " + discount)
     // Controllo che i km siano un numero corretto
     if (isNaN(userDOMKilometers) || userDOMKilometers < 0) {
         alert("i dati non sono validi");
@@ -43,17 +46,25 @@ btnDOMElement.addEventListener('click', function () {
     } else { // Se i dati dei km sono corretti
 
         // Applico i vari sconti
-        if (userDOMDiscount.value == 0) {
-            finalPrice = (standardPrice * SCONTO_MINORENNI) / 100
+        if (userDOMDiscount == 0) {
+            discount = (standardPrice * SCONTO_MINORENNI) / 100
 
-        } else if (userDOMDiscount.value == 2) {
-            finalPrice = (standardPrice * SCONTO_OVER_65) / 100
+        } else if (userDOMDiscount == 2) {
+            discount = (standardPrice * SCONTO_OVER_65) / 100
+        } else {
+            discount = 0
         }
-    
-        // Aggiorno il dom con il prezzo finale
-        let prezzoDOMElement = document.getElementById('price')
-        prezzoDOMElement.innerHTML = '<p>' + finalPrice.toFixed(2) + ' &euro;</p>'
+        
     }
+
+    let finalPrice = standardPrice - discount
+    console.log("prezzo finale " + finalPrice)
+    console.log(typeof finalPrice)
+    
+
+    // Aggiorno il dom con il prezzo finale
+    let prezzoDOMElement = document.getElementById('price')
+    prezzoDOMElement.innerHTML = "<p>" + finalPrice.toFixed(2) + " &euro;</p>" ;
 })
 
 
